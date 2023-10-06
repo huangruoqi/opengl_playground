8c3eec030370eff53694e84bb683a1897218b0e9
Ruoqi Huang <huangruoqi374@gmail.com>
2023-10-05 22:42:34 -0700

    add vaos

--- src/restart/mesh.py
+++ src/restart/mesh.py
@@ -5,12 +5,15 @@ from abc import ABC, abstractclassmethod
 from .utils import load_shaders
 
 class Mesh(ABC):
-    @abstractclassmethod
+    def __init__(self):
+        self.vaos = []
+
     def render(self):
-        raise NotImplementedError
+        [vao.render(moderngl.TRIANGLE_STRIP) for vao in self.vaos]
 
 class Background(Mesh):
     def __init__(self, ctx: moderngl.Context):
+        super(Background, self).__init__()
         self.texture = ctx.texture((1920, 1080), 4, dtype='f1')
         self.texture.use(location=0)
         program = ctx.program(**load_shaders('text'))
@@ -22,10 +25,7 @@ class Background(Mesh):
             1, 1, 1.0, 1.0,
         ], dtype='f4')
         vbo = ctx.buffer((quad).tobytes())
-        self.vao = ctx.simple_vertex_array(program, vbo, 'vert', 'uv')
-
-    def render(self):
-        self.vao.render(moderngl.TRIANGLE_STRIP)
+        self.vaos.append(ctx.simple_vertex_array(program, vbo, 'vert', 'uv'))
 
     def blit(self, surface: pygame.Surface, rect: pygame.Rect):
         text_data: bytes = pygame.image.tostring(surface, "RGBA", True)
